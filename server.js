83e48caae281ffa020c7350b2c86971fd5943cf5
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-05 18:43:07 -0500

    added production scripts and build routes for server.js

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("client/build"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
@@ -26,6 +27,10 @@ connection.on("connected", () => {
 connection.on("error", (err) => {
   console.log("Mongoose connection error: ", err);
 });
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
 
 app.get("/api/config", (req, res) => {
   res.json({

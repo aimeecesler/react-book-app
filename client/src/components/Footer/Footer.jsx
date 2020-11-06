fe8cd9549dfc98f4870be0e2c505c74f5991fbd6
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-05 21:08:08 -0500

    formatted footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer mt-auto py-1">
+    <footer className="footer mt-auto py-1 bg-info">
       <div className="container-fluid">
         <div className="row">
-          <span className="col-sm-4 text-center text-muted my-auto">
+          <span className="col-sm-12 text-white text-center my-auto">
             Copyright&copy; 2020 Aimee Corbin Esler
           </span>
         </div>
--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import Container from "../../components/Container/Container";
 import ContainerFluid from "../../components/ContainerFluid/ContainerFluid";
 import LogoHeader from "../../components/LogoHeader/LogoHeader";
 
 const Search = () => {
   return (
-    <ContainerFluid>
-      <LogoHeader />
-    </ContainerFluid>
+    <>
+      <ContainerFluid>
+        <LogoHeader />
+      </ContainerFluid>
+      <Container>
+          
+      </Container>
+    </>
   );
 };
 

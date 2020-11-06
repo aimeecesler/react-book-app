b17e9b34f6a2a144ce4e0d64f0fd1513194ac7a7
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-05 19:37:33 -0500

    removed unneccessary files and links to them

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import axios from "axios";
 import { useEffect } from "react";
 
@@ -15,20 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TEST</h1>
     </div>
   );
 }

71185d23120616db5b0deb69e524ea9e80cca414
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-05 20:16:47 -0500

    added sticky footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,20 +3,24 @@ import Search from "./pages/Search/Search";
 import Saved from "./pages/Saved/Saved";
 import Book from "./pages/Book/Book";
 import NotFound from "./pages/NotFound/NotFound";
+import Footer from "./components/Footer/Footer";
+import Container from "./components/Container/Container";
 
 function App() {
-
   return (
-    <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={Search} />
-          <Route exact path="/books" component={Saved} />
-          <Route exact path="/book/bookId" component={Book} />
-          <Route path="/" component={NotFound} />
-        </Switch>
+      <div className="d-flex flex-column min-vh-100" >
+        <Container>
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/books" component={Saved} />
+            <Route exact path="/book/bookId" component={Book} />
+            <Route path="/" component={NotFound} />
+          </Switch>
+        </Container>
+        <Footer />
+        </div>
       </Router>
-    </div>
   );
 }
 
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-1">
+      <div className="container-fluid">
+        <div className="row">
+          <span className="col-sm-4 text-center text-muted my-auto">
+            Copyright&copy; 2020 Aimee Corbin Esler
+          </span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
